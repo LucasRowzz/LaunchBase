2f60ced037fc704d40f78e5576e94f3beaccb7fc
Lucas Araujo <lucasaraujo4soares@gmail.com>
2020-01-27 20:12:47 -0300

    Tipos de variáveis e etc

---
+++ index.js
@@ -0,0 +1,16 @@
+// Criar um programa que calcula a média
+// das notas entre os alunos e envia
+// mensagem do cálculo da média.
+
+const nome = "Lucas"
+const nome2 = 'Mayk'
+// Essa abaixo é chamada de template string
+// pois consigo colocar uma variável dentro.
+const nome3 = `Lucas e ${nome2}`
+
+const aluno01 = "Lucas"
+const aluno02 = 'Mayk'
+// Variável tipo number.
+const notaAluno01 = 9.8 
+
+console.log(typeof notaAluno01)
