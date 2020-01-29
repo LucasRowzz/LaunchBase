3f5697128db0981210b76ef23632ce1bc7631f30
Lucas Araujo <lucasaraujo4soares@gmail.com>
2020-01-28 21:09:22 -0300

    Criando os arquivos objetos, vetores e aprendendo utilizalos

---
+++ objeto.js
@@ -0,0 +1,21 @@
+// Objetos
+// Como Criar 
+const aluno01 = {
+    nome: "lucas",
+    nota: 9.8
+}
+
+const aluno02 = {
+    nome: "Diego",
+    nota: 10
+}
+
+const aluno03 = {
+    nome: "Fulano",
+    nota: 2
+}
+
+// Como acessar os objetos 
+const media =(aluno01.nota + aluno02.nota + aluno03.nota) / 3
+
+console.log(media)
---
+++ vetores.js
@@ -0,0 +1,27 @@
+// Criando vetores, serve para colocar diversos objetos ou valores agrupados
+// Arrays ou vetores
+const alunos = [
+    {
+        nome:"Lucas",
+        nota: 9.8
+    },
+    {
+        nome: "Diego",
+        nota: 10
+    },
+    {
+        nome: "Fulano",
+        nota: 2
+    }
+]
+/* Para acessar utilizaremos através da posição
+   Lembrando que sempre começam na posição [0]
+*/
+// Para acessar esses valores como arrays
+const media = (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
+console.log(media)
+
+// Outras formas de utilizar
+
+const nomesDeAlunos = ["Lucas", "Mayk", "Mario"]
+console.log(alunos[1])
