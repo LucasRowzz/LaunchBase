2f447610239708bb7cb059bfb560a9bf6fa6f45d
Lucas Araujo <lucasaraujo4soares@gmail.com>
2020-01-28 22:52:16 -0300

    Aprendendo a usar functions e estruturas de repeticao

---
+++ estrutu-repeticao.js
@@ -0,0 +1,80 @@
+// Estruturas de Repetição
+
+/* Criar um programa que calcula a média
+   das turmas de alunos e envia mensagem
+   do calculo da média 
+*/
+
+const alunosDaTurmaA = [
+    {
+        nome: 'Lucas',
+        nota: 1.8
+    },
+    {
+        nome: 'Jorge',
+        nota: 10
+    },
+    {
+        nome: 'Leticia',
+        nota: 2
+    },
+    {
+        nome : 'Mais um Aluno',
+        nota : 10
+
+    } 
+]
+
+const alunosDaTurmaB = [
+    {
+        nome: 'Amanda',
+        nota: 10
+    },
+    {
+        nome: 'Fernando',
+        nota: 10
+    },
+    {
+        nome: 'Robson',
+        nota: 0
+    },
+    {
+        nome : 'Novo Aluno',
+        nota : 5
+
+    }       
+]
+
+
+function calculaMedia(alunos) {
+    // Estruturas de Repetição
+
+    /* let, ao contrário de const, pode ser alterado o valor.
+       então criamos essa variável: let i = 0; e depois dizemos
+       o seguinte, se i, for menor que a quantidade de alunos 
+       (i < alunos.length) ele vai incrementar mais um (i++) que
+       significa a mesma coisa de i = i + 1  */
+       
+
+       let soma = 0;
+       for (let i = 0; i < alunos.length; i++) {
+           soma = soma + alunos[i].nota
+       }
+       const media = soma / alunos.length
+       return media
+}
+
+const media1 = calculaMedia(alunosDaTurmaA)
+const media2 = calculaMedia(alunosDaTurmaB)
+
+function enviaMensagem(media, turma){
+    // Se a média for maior que 5, parabenizar a turma
+    if (media > 5) {
+        console.log(`A média da turma ${turma} ${media}. Parabéns`)
+    } else{
+        console.log(`A média da turma ${turma} é menor que 5`)
+    }
+}
+            // Parâmetros da função
+enviaMensagem(media1, 'TurmaA')
+enviaMensagem(media2, 'TurmaB')
